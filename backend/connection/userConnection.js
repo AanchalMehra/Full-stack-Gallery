6f2090d3cb38be09272fd964f865897dec4c604f
AanchalMehra <aanchalmehra15@gmail.com>
2026-05-05 23:03:44 +0530

    fixed IMPORTS

--- backend/connection/userConnection.js
+++ backend/connection/userConnection.js
@@ -1,8 +1,8 @@
-const mongoose=require("mongoose");
+import mongoose from "mongoose";
 const dbConnection=(url)=>{
     mongoose
     .connect(url)
     .then(()=>console.log("DATABASE connected successfully"))
     .catch((err)=>console.log("Error occured:",err.message))
 }
-module.exports=dbConnection;
+export default dbConnection;
